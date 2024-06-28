ec9b59602fc0b1bd415f9905bb3831ce677d1edb
David Salazar González <142959884+WillieAnthony@users.noreply.github.com>
2024-06-27 23:54:54 -0600

    Suma De Dos Numeros Python
    Código en lenguaje Python que solicite al usuario que ingrese dos números, los sume y muestre el resultado

---
+++ Suma De Dos Numeros.py
@@ -0,0 +1,24 @@
+# Codigo en lenguaje Python que solicite al usuario que ingrese dos numeros, los sume y muestre el resultado
+
+# Solicitar al usuario que ingrese dos numeros
+print("SUMA DE DOS NUMEROS\n\n")
+
+# Ingresar el primer numero
+num1 = float(input("Ingrese El Primer Numero: \n"))
+
+print("\n")
+
+# Ingresar el segundo numero
+num2 = float(input("Ingrese El Segundo Numero: \n"))
+
+# Sumar los dos numeros
+resultado = num1 + num2
+
+print("\n\n")
+
+# Mostrar el resultado
+print(f"La Suma de {num1:.2f} y {num2:.2f} es: {resultado:.2f}\n\n\n\n\n")
+
+print("27/06/2024")
+print("David Salazar Gonzalez")
+print("Willie Anthony")
